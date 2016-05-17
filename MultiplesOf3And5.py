8dc1a97b3f382406b6976f0c0f7b4625530dc927
Christian Borst <christianoborst@gmail.com>
2016-05-17 15:54:57 -0500

    Completed Multiples of 3 and 5

--- MultiplesOf3And5.py
+++ MultiplesOf3And5.py
@@ -4,4 +4,13 @@
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
+#sigma sums the numbers from 1 to end (inclusive)
+def sigma(end):
+  #Sum identity
+  return (end * (end + 1) / 2)
 
+sum3 = 3 * sigma(333)
+sum5 = 5 * sigma(199)
+sum15 = 15 * sigma(66)
+
+print sum3 + sum5 - sum15
